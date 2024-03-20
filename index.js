814df59ce9b95f173c6c0f2183e09cfe3456f2cb
aspcodenet <stefan.holmberg@systementor.se>
2024-03-20 08:26:29 +0100

    a

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// KOM IHÅG npx sequelize-cli db:seed:all.
+//så¨ni får in lite produkter
+
 const express = require('express')
 const {check} = require('express-validator')
 const cors = require('cors')
@@ -14,71 +17,75 @@ app.use(cors({
     credentials:true 
 }))
 
-app.get('/products', check('q').escape() , async(req,res)=>{
+
+app.get('/api/products',  async(req,res)=>{
+    const allProducts = await Product.findAll();
+    return res.json(allProducts);
+});
+
+
+// app.get('/products', check('q').escape() , async(req,res)=>{
     
     
-    //console.log(req.query.sortCol)
-    //console.log(req.query.sortOrder)
-    const sortCol =  req.query.sortCol || 'id';
-    const sortOrder =  req.query.sortOrder || 'asc';
-    const q =  req.query.q || '';
-    const offset =  Number(req.query.offset || 0);
-    //const page =  req.query.offset || 0;
-    const limit =  Number(req.query.limit || 20);
-    // if(req.query.sortCol === undefined){
-    //     sortCol = 'id'
-    // }else{
-    //     sortCol = req.query.sortCol
-    // }
+//     //console.log(req.query.sortCol)
+//     //console.log(req.query.sortOrder)
+//     const sortCol =  req.query.soquery.offset || 0);
+//     //const page =  req.query.offset || 0;
+//     const limit =  Number(req.query.limit || 20);
+//     // if(req.query.sortCol === undefined){
+//     //     sortCol = 'id'
+//     // }else{
+//     //     sortCol = req.query.sortCol
+//     // }
 
 
-    // SELECT * from Product order by id asc 
-    const allProducts = await Product.findAll({
-        order: [ 
-            [sortCol, sortOrder]
-         ],
-    })
-    const result2 = allProducts.map(p=>{
-        return {
-           id:p.id,
-           name:p.name,
-           unitPrice:p.unitPrice,
-           stockLevel:p.stockLevel
-       }
-    })
-    res.json(result2)
+//     // SELECT * from Product order by id asc 
+//     const allProducts = await Product.findAll({
+//         order: [ 
+//             [sortCol, sortOrder]
+//          ],
+//     })
+//     const result2 = allProducts.map(p=>{
+//         return {
+//            id:p.id,
+//            name:p.name,
+//            unitPrice:p.unitPrice,
+//            stockLevel:p.stockLevel
+//        }
+//     })
+//     res.json(result2)
 
     
 
 
 
-    // const allProducts = await Product.findAndCountAll({
-    //     where:{
-    //         name:{
-    //             [Op.like]: '%' + q + '%'
-    //         }
-    //     },
-    //     order: [ 
-    //         [sortCol, sortOrder]
-    //      ],
-    //     offset: offset,
-    //     limit:limit
+//     // const allProducts = await Product.findAndCountAll({
+//     //     where:{
+//     //         name:{
+//     //             [Op.like]: '%' + q + '%'
+//     //         }
+//     //     },
+//     //     order: [ 
+//     //         [sortCol, sortOrder]
+//     //      ],
+//     //     offset: offset,
+//     //     limit:limit
 
-    // })
-    // const total = allProducts.count
-    // const result = allProducts.rows.map(p=>{
-    //     return {
-    //        id:p.id,
-    //        name:p.name,
-    //        unitPrice:p.unitPrice,
-    //        stockLevel:p.stockLevel
-    //    }
-    // })
-    // return res.json({
-    //     total,
-    //     result
-    // })
-})
+//     // })
+//     // const total = allProducts.count
+//     // const result = allProducts.rows.map(p=>{
+//     //     return {
+//     //        id:p.id,
+//     //        name:p.name,
+//     //        unitPrice:p.unitPrice,
+//     //        stockLevel:p.stockLevel
+//     //    }
+//     // })
+//     // return res.json({
+//     //     total,
+//     //     result
+//     // })
+// })
 
 
 app.listen(port, async () => {
